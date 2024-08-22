3f5fb3d255f54f7ec64abfb16369c50916665628
Shijith <shijith.s@optym.com>
2024-08-22 12:33:37 +0530

    Converted Routeview component to class component

--- src/pages/RoutesView.js
+++ src/pages/RoutesView.js
@@ -1,32 +1,56 @@
 import RoutesGrid from "components/RoutesGrid";
 import RoutesMap from "components/RoutesMap";
 import StopsGrid from "components/StopsGrid";
-import React, { useEffect } from "react";
 import "styles/routesView.css";
-import store from "store";
-import { observer } from "mobx-react";
 
-const RoutesView = () => {
-  useEffect(() => {
-    console.log("populating data");
-    store.populateRouteAndStopsData();
-  }, []);
+import React, { Component } from "react";
 
-  console.log("store is : ", store);
+class RoutesView extends Component {
+  // componentDidMount() {
+  // console.log("populating data");
+  // this.props.store.populateRouteAndStopsData();
+  // }
 
-  return (
-    <div className="routesview">
-      <div className="routesview_item1">
-        <RoutesGrid />
+  render() {
+    return (
+      <div className="routesview">
+        <div className="routesview_item1">
+          <RoutesGrid />
+        </div>
+        <div className="routesview_item2">
+          <RoutesMap />
+        </div>
+        <div className="routesview_item3">
+          <StopsGrid />
+        </div>
       </div>
-      <div className="routesview_item2">
-        <RoutesMap />
-      </div>
-      <div className="routesview_item3">
-        <StopsGrid />
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default RoutesView;
+
+// const RoutesView = () => {
+//   useEffect(() => {
+//     console.log("populating data");
+//     store.populateRouteAndStopsData();
+//   }, []);
+
+//   console.log("store is : ", store);
+
+//   return (
+//     <div className="routesview">
+//       <div className="routesview_item1">
+//         <RoutesGrid />
+//       </div>
+//       <div className="routesview_item2">
+//         <RoutesMap />
+//       </div>
+//       <div className="routesview_item3">
+//         <StopsGrid />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default RoutesView;
